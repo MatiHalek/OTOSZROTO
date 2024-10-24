630115776cf03b82155e0528b3b36589b18c577e
Uzyxen <michal912456@gmail.com>
2024-10-24 16:10:12 +0200

    styles: decrease x-axios padding in AppForm

--- components/UI/AppForm.vue
+++ components/UI/AppForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <form class="w-1/3 bg-[#FFF] h-screen px-24 pt-32">
+    <form class="w-1/3 bg-[#FFF] h-screen px-20 pt-32">
         <slot />
     </form>
 </template>
--- components/inputs/AppInput.vue
+++ components/inputs/AppInput.vue
@@ -1,5 +1,5 @@
 <template>
-    <input :type="type" class="outline-none border-solid border-2 border-[#DDD] text-base p-1.5">
+    <input :type="type" class="outline-none border-solid border-2 border-[#DDD] text-base p-2">
 </template>
 
 <script setup>
