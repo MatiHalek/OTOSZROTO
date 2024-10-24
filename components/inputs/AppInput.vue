4d4a0a6f038ec7cdb71026795e7185452397988d
Uzyxen <michal912456@gmail.com>
2024-10-24 16:52:59 +0200

    add focus effect on inputs

--- components/inputs/AppInput.vue
+++ components/inputs/AppInput.vue
@@ -1,5 +1,5 @@
 <template>
-    <input :type="type" class="outline-none border-solid border-2 border-[#DDD] text-base p-2">
+    <input :type="type" class="outline-none border-solid border-2 border-[#DDD] text-base p-2 focus:border-[#E5A00A]">
 </template>
 
 <script setup>
