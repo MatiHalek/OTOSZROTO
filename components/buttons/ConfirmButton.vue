e6526bd08d94fbe6109e71d14efae2eb235db5d9
Vistaaa <mateusz.marmuzniak.poland@gmail.com>
2024-10-26 01:46:45 +0200

    Add small improvements to form buttons and titles

--- components/buttons/ConfirmButton.vue
+++ components/buttons/ConfirmButton.vue
@@ -1,6 +1,8 @@
 <template>
-    <button class="relative flex h-[50px] items-center justify-center overflow-hidden bg-[#E5A00A] text-[#FFF] p-2.5 mt-20 rounded-lg shadow-xl transition-all duration-300 before:absolute before:inset-0 before:border-[0px] before:border-white before:transition-all before:duration-300 before:ease-linear hover:bg-white hover:text-blue-600 hover:shadow-blue-600 hover:before:border-[25px]">
-  <span class="relative z-10"><slot/></span>
+    <button class="relative flex h-[50px] items-center justify-center overflow-hidden bg-[#E5A00A] text-[#FFF] p-2.5 mt-20 rounded-lg shadow-xl transition-all duration-300 before:absolute before:inset-0 before:border-[0px] before:border-white before:transition-all before:duration-300 before:ease-linear hover:bg-white hover:text-[#463691] hover:shadow-[#46369188] hover:before:border-[25px]">
+  <span class="relative z-10">
+    <slot/>
+</span>
 </button>
 
 </template>
