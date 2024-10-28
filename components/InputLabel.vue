0443472237ddede9f3bddea0a03527cd398dd362
Uzyxen <michal912456@gmail.com>
2024-10-28 21:39:08 +0100

    fix: change props name to avoid name collision

--- components/InputLabel.vue
+++ components/InputLabel.vue
@@ -1,9 +1,9 @@
 <template>
-    <label :for="for" class="pb-2 text-[#332404]">
+    <label :for="propFor" class="pb-2 text-[#332404]">
         <slot />
     </label>
 </template>
 
 <script setup>
-    defineProps(['for']);
+    defineProps(['propFor']);
 </script>
