2f290d5a686ba081172b74add698852b93aa63f7
Kacper Piaskowy <kacperpiaskowy937@gmail.com>
2024-10-30 16:09:58 +0100

    Add user repository interface

--- api/Data/AppDbContext.cs
+++ api/Data/AppDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; } = default!;
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
---
+++ api/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using api.Models;
+
+namespace api.Interfaces
+{
+    public interface IUserRepository
+    {
+        User Create(User user); 
+        User Update(User user); 
+        User Get(int userID); 
+        List<User> GetAll(); 
+        int Remove(int userID);
+    }
+}
