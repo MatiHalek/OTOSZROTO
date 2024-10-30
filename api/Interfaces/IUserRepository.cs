f68e0894fcfc538b82f09cd1d987b3eca614e82e
Kacper Piaskowy <kacperpiaskowy937@gmail.com>
2024-10-30 16:14:52 +0100

    create user repository

--- api/Interfaces/IUserRepository.cs
+++ api/Interfaces/IUserRepository.cs
@@ -5,7 +5,7 @@ namespace api.Interfaces
     public interface IUserRepository
     {
         User Create(User user); 
-        User Update(User user); 
+        User Update(User user,int userID); 
         User Get(int userID); 
         List<User> GetAll(); 
         int Remove(int userID);
