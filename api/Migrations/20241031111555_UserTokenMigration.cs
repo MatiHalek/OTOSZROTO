72d417cbe2574c0ec4e09d8094c6300f6142ae1f
Kacper Piaskowy <kacperpiaskowy937@gmail.com>
2024-10-31 12:38:39 +0100

    Create user token database model and add new migration

---
+++ api/Migrations/20241031111555_UserTokenMigration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserTokenMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserTokens",
+                columns: table => new
+                {
+                    UserTokenID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Token = table.Column<string>(type: "text", nullable: true),
+                    UserID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTokens", x => x.UserTokenID);
+                    table.ForeignKey(
+                        name: "FK_UserTokens_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserTokens_UserID",
+                table: "UserTokens",
+                column: "UserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserTokens");
+        }
+    }
+}
---
+++ api/Models/UserToken.cs
@@ -0,0 +1,10 @@
+﻿namespace api.Models
+{
+    public class UserToken
+    {
+        public int UserTokenID { get; set; }
+        public string? Token { get; set; }
+        public int UserID { get; set; }
+        public User User { get; set; } = null!;
+    }
+}
