e2b7aca1b90c4697ae659ab61c050bfaefc65e0e
Uzyxen <michal912456@gmail.com>
2024-10-31 13:53:48 +0100

    feat: login user

--- frontend/pages/logowanie/index.vue
+++ frontend/pages/logowanie/index.vue
@@ -4,18 +4,18 @@
         <VerticalGroup class="gap-y-6 relative overflow-auto grow">         
             <VerticalGroup>
                 <InputLabel for="loginInputEmail">Adres e-mail</InputLabel>
-                <AppInput :validate="false" class="shadow-xl" type="email" id="loginInputEmail" />
+                <AppInput v-model="userData.Email" :validate="false" class="shadow-xl" type="email" id="loginInputEmail" />
             </VerticalGroup>
 
             <VerticalGroup>
                 <InputLabel for="loginInputPassword">Hasło</InputLabel>
-                <AppInput :validate="false" class="shadow-xl" type="password" id="loginInputPassword" />
+                <AppInput v-model="userData.Password" :validate="false" class="shadow-xl" type="password" id="loginInputPassword" />
             </VerticalGroup>
             <p class="text-right">
                 <NuxtLink :to="'/przypomnij-haslo'" class="text-[#463691] font-medium">Nie pamiętam hasła</NuxtLink>
             </p>
 
-            <ConfirmButton class="shadow-xl">Zaloguj się</ConfirmButton>
+            <ConfirmButton class="shadow-xl" @click.prevent="loginUser()">Zaloguj się</ConfirmButton>
             <p class="text-center">
                 <span>Nie masz konta? </span>
                 <NuxtLink :to="'/rejestracja'" class="text-[#463691]">Zarejestruj się</NuxtLink>
@@ -32,6 +32,26 @@
     useHead({
         title: 'Logowanie | OTOSZROTO'
     })
+
+    const userData = ref({
+        Email: '',
+        Password: ''
+    });
+
+    async function loginUser() {
+        const response = await $fetch('http://localhost:5271/api/auth/login', {
+            method: 'POST',
+            body: {
+                "email": userData.value.Email,
+                "password": userData.value.Password
+            },
+            credentials: 'include'
+        })
+
+        if(response.message == 'success') {
+            await navigateTo('/');
+        }
+    }
 </script>
 
 <style scoped>
