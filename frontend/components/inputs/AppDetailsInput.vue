cd626f1f9127139ef736b29d7ede078b9da79cb8
Uzyxen <michal912456@gmail.com>
2024-11-01 12:47:55 +0100

    create add-offer page

--- frontend/components/inputs/AppDetailsInput.vue
+++ frontend/components/inputs/AppDetailsInput.vue
@@ -1,11 +1,10 @@
 <template>
-    <input type="text" v-model="inputValue">
+    <input 
+        type="text" 
+        v-model="inputValue"
+        class="outline-none border-[#DDD] border-2 w-full p-1 h-10">
 </template>
 
 <script setup>
     const inputValue = defineModel();
 </script>
-
-<style scoped>
-
-</style>
