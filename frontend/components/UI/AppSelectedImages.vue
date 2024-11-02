2be13e69586b37ababc3055ace817b465b7bffb7
Uzyxen <michal912456@gmail.com>
2024-11-02 15:00:59 +0100

    create image component

---
+++ frontend/components/UI/AppSelectedImages.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex">
+         <h2>{{ image.name }}</h2>
+         <h3>{{ formatFileSize(image.size) }}</h3>
+    </div>
+</template>
+
+<script setup>
+    defineProps(['image']);
+
+    function formatFileSize(bytes) {
+        if (bytes >= 1024 * 1024) {
+            return `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
+        } else {
+            return `${(bytes / 1024).toFixed(2)} KB`;
+        }
+    }
+</script>
