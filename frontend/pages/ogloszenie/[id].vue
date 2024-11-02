844274b998fe60f085972de6eeadff5d5989612e
Vistaaa <matimarmuzniak@gmail.com>
2024-11-02 12:58:30 +0100

    Add image gallery, add other car data to table

--- frontend/pages/ogloszenie/[id].vue
+++ frontend/pages/ogloszenie/[id].vue
@@ -1,7 +1,31 @@
 <template>
     <section class="bg-[#F3F3F3] pb-36 md:pb-0">
-	    <img class="w-full h-[450px] object-cover" src="https://wallpaperaccess.com/full/1673840.jpg">
-        <article class="container mx-auto px-4 lg:px-8" id="advertisementInfo">
+        <div class="relative w-full h-[450px] overflow-hidden">
+            <div class="absolute inset-0 w-full h-full overflow-hidden">
+            <img
+                :src="images[currentIndex]"
+                alt="Rozmyte tło"
+                class="w-full h-full object-cover blur-md scale-110"
+            />
+            </div>
+            <div class="relative w-full h-full flex items-center justify-center overflow-hidden">
+            <div class="flex transition-transform duration-500 h-full"
+                :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
+                <div v-for="(image, index) in images" :key="index" class="min-w-full h-full flex justify-center">
+                <img :src="image" alt="Obrazek galerii" class="rounded-xl h-full w-full max-w-[1200px] object-cover" />
+                </div>
+            </div>
+            </div>
+            <button @click="prevImage"
+                    class="w-10 h-10 shadow-lg absolute top-1/2 left-4 transform -translate-y-1/2 bg-[#1e90ff] bg-opacity-75 text-white px-2 py-1 rounded-full hover:bg-opacity-100">
+                <i class="fa-solid fa-caret-left"></i>
+            </button>
+            <button @click="nextImage"
+                    class="w-10 h-10 shadow-lg absolute top-1/2 right-4 transform -translate-y-1/2 bg-[#1e90ff] bg-opacity-75 text-white px-2 py-1 rounded-full hover:bg-opacity-100">
+                <i class="fa-solid fa-caret-right"></i>
+            </button>
+        </div>
+        <article class="relative container mx-auto px-4 lg:px-8" id="advertisementInfo">
             <section class="grid grid-cols-1 md:grid-cols-[minmax(0,_2.5fr)_minmax(250px,_1fr)] grid-flow-dense gap-4 -mt-16">
                 <section class="flex flex-col items-center justify-center space-y-4 rounded-lg bg-[#D0D0D090] backdrop-blur-md md:backdrop-blur-none md:bg-white p-3 break-all shadow-lg md:col-start-2 md:row-start-1 row-span-5 h-fit min-h-[8rem] fixed md:sticky md:top-24 break-normal bottom-0 w-full start-0">
                     <a href="mailto:" class="flex relative min-h-[50px] items-center justify-center overflow-hidden text-[#FFF] p-2.5 rounded-lg shadow-xl transition-all duration-300 before:absolute before:inset-0 before:border-[0px] before:border-white before:transition-all before:duration-300 before:ease-linear hover:bg-white hover:text-[#463691] hover:shadow-special hover:before:border-[25px] font-bold bg-[#1e90ff] mt-0 shadow-xl w-11/12 cursor-pointer">
@@ -99,16 +123,24 @@
                     <table class="w-full">
                         <tbody>
                             <tr>
-                                <td class="p-3 font-bold">data1</td>
-                                <td class="p-3">data2</td>
+                                <td class="p-3 font-bold">Model</td>
+                                <td class="p-3">model</td>
                             </tr>
                             <tr class="bg-[#EEE]">
-                                <td class="p-3 font-bold">data3</td>
-                                <td class="p-3">data4</td>
+                                <td class="p-3 font-bold">Kolor</td>
+                                <td class="p-3">color</td>
                             </tr>
                             <tr>
-                                <td class="p-3 font-bold">data5</td>
-                                <td class="p-3">data6</td>
+                                <td class="p-3 font-bold">Liczba drzwi</td>
+                                <td class="p-3">numberOfDoors</td>
+                            </tr>   
+                            <tr class="bg-[#EEE]">
+                                <td class="p-3 font-bold">Liczba miejsc</td>
+                                <td class="p-3">numberOfPlaces</td>
+                            </tr>
+                            <tr>
+                                <td class="p-3 font-bold">VIN</td>
+                                <td class="p-3">VIN</td>
                             </tr>   
                         </tbody>                    
                     </table>
@@ -125,4 +157,21 @@
     useHead({
         title: '[tytuł] | OTOSZROTO'
     });
+
+    const images = ref([
+    "https://via.placeholder.com/800x450",
+    "https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg",
+    "https://wallpaperaccess.com/full/1673840.jpg"
+    ]);
+
+    const currentIndex = ref(0);
+
+    function nextImage() {
+    currentIndex.value = (currentIndex.value + 1) % images.value.length;
+    }
+
+    function prevImage() {
+    currentIndex.value =
+        (currentIndex.value - 1 + images.value.length) % images.value.length;
+    }
 </script>
