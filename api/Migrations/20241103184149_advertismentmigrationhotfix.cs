949c0d7ea4fbe5e9e8fab23aa98b787da82bbef5
Kacper Piaskowy <kacperpiaskowy937@gmail.com>
2024-11-03 19:42:27 +0100

    hot fix advertisment model migration

---
+++ api/Migrations/20241103184149_advertismentmigrationhotfix.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class advertismentmigrationhotfix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Advertisements",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPriceNegotiable",
+                table: "Advertisements",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Advertisements");
+
+            migrationBuilder.DropColumn(
+                name: "IsPriceNegotiable",
+                table: "Advertisements");
+        }
+    }
+}
