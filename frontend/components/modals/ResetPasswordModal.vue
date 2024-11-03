a18156bc21fde330d7a112a50fed53d17c8912b6
Uzyxen <michal912456@gmail.com>
2024-11-03 21:44:15 +0100

    send api request

--- frontend/components/modals/ResetPasswordModal.vue
+++ frontend/components/modals/ResetPasswordModal.vue
@@ -7,9 +7,9 @@
                         <h2 class="text-center font-bold text-3xl">Nie pamiętasz hasła?</h2>
                         <h3 class="text-center py-3 text-base">Na podany e-mail prześlemy link do zmiany hasła!</h3>
 
-                        <input class="outline-none border-2 w-full mt-8 p-2" type="text" placeholder="E-mail">
+                        <input v-model="email" class="outline-none border-2 w-full mt-8 p-2" type="text" placeholder="E-mail">
                         <div class="flex flex-col mt-10 gap-3">
-                            <button class="w-full bg-[#E5A00A] p-2 text-[#FFF]">Wyślij link</button>
+                            <button class="w-full bg-[#E5A00A] p-2 text-[#FFF]" @click="sendEmail()">Wyślij link</button>
                             <button @click="$emit('close')" class="w-full bg-[#DDD] p-2">Anuluj</button>
                         </div>
                     </div>
@@ -21,6 +21,16 @@
 
 <script setup>
     const props = defineProps(['isVisible']);
+
+    const email = ref();
+
+    async function sendEmail() {
+        const response = await $fetch(`http://localhost:5271/api/auth/reset/${email.value}`, {
+            method: 'post'
+        });
+
+        console.log(response);
+    }
 </script>
 
 <style scoped>
