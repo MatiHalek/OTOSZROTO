1e258e82731096ed0341dfe86673d71b1ab83010
Uzyxen <michal912456@gmail.com>
2024-11-05 17:31:51 +0100

    add dropdown

--- frontend/components/UI/Header.vue
+++ frontend/components/UI/Header.vue
@@ -13,12 +13,25 @@
             <NuxtLink :to="'/rejestracja'" class="p-1.5 bg-[#E5A00A] text-[#FFF]">Utwórz konto</NuxtLink>
         </div>
 
-        <div v-else>
-            <NuxtLink :to="'/profil'" class="p-2.5 px-5 bg-[#E5A00A] text-[#FFF] text-base">Moje konto</NuxtLink>
+        <div v-else class="relative">
+            <button class="p-2.5 px-5 bg-[#E5A00A] text-[#FFF] text-base" @click="isCollapsed = !isCollapsed">Moje konto</button>
+
+            <div class="absolute bg-white p-3 border-2 border-t-0" v-show="isCollapsed">
+                <ul class="flex flex-col gap-3">
+                    <li>
+                        placeforemail@gmail.com
+                    </li>
+
+                    <li>
+                        <button class="bg-red-500 text-[#FFF] p-2 w-full">Wyloguj się</button>
+                    </li>
+                </ul>
+            </div>
         </div>
     </header>
 </template>
 
 <script setup>
-    const isLogged = ref(false);
+    const isLogged = ref(true);
+    const isCollapsed = ref(false);
 </script>
