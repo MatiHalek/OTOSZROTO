82f9fb1d72e5b4efe2340a770d2e57c518c6dcfa
Uzyxen <michal912456@gmail.com>
2024-11-05 19:35:02 +0100

    create featured offers section

---
+++ frontend/components/UI/AppOffer.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="w-[600px] h-[300px] bg-[#EAEAEA]">
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="mt-20 flex flex-col items-center gap-[50px]">
         <AppMainHeader />
-
         <AppSearchBlock />
+
+        <h2 class="text-center text-3xl mt-40 font-semibold">wyróżnione oferty</h2>
+
+        <AppFeaturedOffers />
     </div>
 </template>
 
