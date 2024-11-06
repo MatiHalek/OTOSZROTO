43824d640a65a831a792420b8c11194806998f75
Uzyxen <michal912456@gmail.com>
2024-11-06 23:06:01 +0100

    get 5 newest offers

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -5,7 +5,7 @@
         <h2 class="text-center text-3xl mt-40 font-semibold">Najnowsze ogłoszenia</h2>
 
         <HorizontalGroup class="gap-4">
-            <AppOfferBlock v-for="i in 5" />
+            <AppOfferBlock v-for="offer in newestOffers" />
         </HorizontalGroup>
     </div>
 </template>
@@ -14,6 +14,10 @@
     useHead({
         title: 'OTOSZROTO, czyli jazda zaczyna się tutaj | Strona główna',
     });
+
+    const offers = useOffersStore();
+    const newestOffers = ref();
+    newestOffers.value = offers.offers.sort((a, b) => b.advertisementID - a.advertisementID).slice(0, 5);
 </script>
 
 <style scoped>
