258939b2e0e22c60707087fd3596ad09bba88e99
Uzyxen <michal912456@gmail.com>
2024-11-06 23:56:47 +0100

    filter offers by search value and display in v-for

--- frontend/pages/szukaj/[value].vue
+++ frontend/pages/szukaj/[value].vue
@@ -1,9 +1,18 @@
 <template>
-    <div>
-        {{ value }}
+    <div class="container mx-auto">
+        <h2 class="text-2xl text-center py-8">Wyniki wyszukiwania dla: {{ value }} ({{ filteredOffers.length }})</h2>
+
+        <HorizontalGroup class="gap-4">
+            <AppOfferBlock v-for="offer in filteredOffers" :offer="offer"/>
+        </HorizontalGroup>
     </div>
 </template>
 
 <script setup>
+    const offers = useOffersStore();
+
     const { value } = useRoute().params;
+    const filteredOffers = offers.offers.filter(item => 
+        item.title.toLowerCase().includes(value.toLowerCase())
+    );
 </script>
