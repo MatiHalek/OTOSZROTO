020f8143deff9694e829669c7c91b6d389ffe47c
Uzyxen <michal912456@gmail.com>
2024-11-07 20:28:22 +0100

    add condition

--- frontend/components/UI/AppOfferBlock.vue
+++ frontend/components/UI/AppOfferBlock.vue
@@ -1,7 +1,8 @@
 <template>
     <NuxtLink :to="'/ogloszenie/' + offer.advertisementID" class="hover:translate-y-[-8px] transition-all duration-500">
         <div class="w-80 aspect-square rounded-lg shadow-lg">
-            <img :src="'http://localhost:5271/api/Uploads/gallery/' + image[0].imageSource" alt="Zdjęcie oferty" class="object-cover rounded-t-lg w-full h-2/4">
+            <img v-if="image[0]" :src="'http://localhost:5271/api/Uploads/gallery/' + image[0].imageSource" alt="Zdjęcie oferty" class="object-cover rounded-t-lg w-full h-2/4">
+            <img v-else alt="Zdjęcie oferty" class="object-cover rounded-t-lg w-full h-2/4">
             <div class="p-3 flex flex-col h-2/4">
                 <p :title="offer.title" class="font-bold text-lg text-ellipsis overflow-hidden w-[calc(100%)] whitespace-nowrap">{{ offer.title }}</p>
                 <hr class="bg-black my-3">
